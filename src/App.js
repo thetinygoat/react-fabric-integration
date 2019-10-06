16deb6d3a5c05ac912e9da18d18ca3ff9b780117
Sachin Saini <iamsainisachin@gmail.com>
2019-10-06 21:44:07 +0530

    integrate fabric js with react and socket io

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect, useRef } from "react";
+import { fabric } from "fabric";
+import openSocket from "socket.io-client";
+import Peer from "simple-peer";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let socket = openSocket("http://localhost:8080");
+  const canvas = new fabric.Canvas("canvas", {
+    height: window.innerHeight / 2,
+    width: window.innerWidth,
+    isDrawingMode: true
+  });
+  socket.on("rdraw", ({ data }) => {
+    console.log();
+    canvas.loadFromJSON(data);
+  });
+  canvas.on("mouse:move", options => {
+    socket.emit("draw", { data: JSON.stringify(canvas) });
+  });
+  return <div></div>;
 }
 
 export default App;
